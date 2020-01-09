d5e8eb9f463d5d5833710f754bc481518bd7a7fb
Thiago da Costa Pinto Frazão de Sousa <f514189@corp.caixa.gov.br>
2020-01-09 16:03:31 -0300

    solving autowired bug

--- src/main/java/crud/hsqldb/utils/PasswordUtils.java
+++ src/main/java/crud/hsqldb/utils/PasswordUtils.java
@@ -1,14 +1,12 @@
 package crud.hsqldb.utils;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PasswordUtils {
 	
-	@Autowired
-	private BCryptPasswordEncoder passEncoder;
+	private BCryptPasswordEncoder passEncoder = new BCryptPasswordEncoder();
 	
 	public String hashPassword(String password) {
 		return passEncoder.encode(password);
